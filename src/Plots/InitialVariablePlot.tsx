8f577db35233fa883759ffef7dd6ab7900c26616
Jdgronewold <jeff.gronewold@gmail.com>
2019-09-24 20:05:36 -0600

    little triangle guys

--- src/Plots/InitialVariablePlot.tsx
+++ src/Plots/InitialVariablePlot.tsx
@@ -32,18 +32,32 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
         return transitionHeight > currPos.y
       })
       let phasePercentage: number
-      if (transitionHeights[calculatedPhase + 1]) {
-        phasePercentage = 1 - (transitionHeights[calculatedPhase] - currPos.y) / (transitionHeights[calculatedPhase + 1] - transitionHeights[calculatedPhase])
+      if (transitionHeights[calculatedPhase - 1]) {
+        phasePercentage = 1 - ((transitionHeights[calculatedPhase] - currPos.y) / (transitionHeights[calculatedPhase] - transitionHeights[calculatedPhase - 1]))  
       } else {
         phasePercentage = 1
       }
       
-      setTransitionPhase({ phaseIndex: calculatedPhase, phasePercentage: phasePercentage })
-    }, [], null, true, 20
+      setTransitionPhase({ phaseIndex: calculatedPhase, phasePercentage: Math.min(1, phasePercentage) })
+    }, [], null, true, 10
   )
 
   useEffect(() => {
     if (svgRef && svgRef.current) {
+
+      const determineTriangleWith = (data: HouseData, percentageWidth: number): number => {
+        const dataHeightPercentage = data.elevation / elevationMinMax[1]
+        const dataHeightRoundedDown = (Math.floor(dataHeightPercentage * 100) / 100)
+        const dataHeightRoundedUp = (Math.ceil(dataHeightPercentage * 100) / 100)
+
+        if (percentageWidth < dataHeightRoundedDown) {
+          return 1
+        } else if (percentageWidth > dataHeightRoundedUp) {
+          return 0
+        } else {
+          return (dataHeightRoundedUp - percentageWidth) / .05
+        }
+      }
       
       const svgElement = d3.select(svgRef.current)
       svgElement.attr('width', props.width).attr('height', props.height)
@@ -54,39 +68,72 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
         .attr('transform', 'translate(25, 25)')
       
       const percentageY = transitionPhase.phaseIndex === 1 ? transitionPhase.phasePercentage : 1
+
       const percentageWidth = transitionPhase.phaseIndex === 2 ?
-        1 - transitionPhase.phasePercentage :
+        transitionPhase.phasePercentage :
         transitionPhase.phaseIndex > 2 ? 0 : 1
-      const percentageX = transitionPhase.phaseIndex >= 3 ? transitionPhase.phasePercentage : 0
-      const roundedCorners = transitionPhase.phaseIndex >= 3 ? 6 : 0
-      const opacity = transitionPhase.phaseIndex < 1 ? 0 : 1
-
       
+      const percentageX = transitionPhase.phaseIndex >= 3 ? transitionPhase.phasePercentage : 0
 
+      const roundedCorners = transitionPhase.phaseIndex >= 3 ? 6 : 0
+      
+      const opacity = transitionPhase.phaseIndex < 1 ? 0 : 0.8
+      
       containerElement.selectAll('g')
-        .data(props.data, (data: HouseData) => `${data.elevation} ${data.price}`)
+        .data(props.data, (data: HouseData, i: number) => `${data.elevation} + ${i}`)
         .join((enter) => {
           return enter.append('g')
-                      .attr('transform', (d: HouseData) => {
-                        const translateX = d.in_sf ?
-                          (0 + percentageX * sqFtSFScale(d.price_per_sqft))  :
-                          (props.width - 25 - (sqFtNYScale(d.price_per_sqft) * percentageX))
-                        const barHeight = props.height - (elevationScale(d.elevation) * percentageY)
-                        const rotateAmount = d.in_sf ? 0 : 180
-                        return `translate(${translateX}, ${barHeight}) rotate(${rotateAmount})`
-                      })
-                      .append('rect')
-                      .attr('height', 6)
-                      .attr('width', Math.max((percentageWidth * (props.width - 30) / 2), 6))
-                      .attr('fill', (data: HouseData) => data.in_sf ? 'green' : 'blue')
-                      .attr('opacity', opacity)
-                      .attr('rx', roundedCorners)
-                      .attr('ry', roundedCorners)
+                      .append('rect')   
+                      .attr('height', 6)       
+                    },
+          (update) => {
+            return update.attr('transform', (d: HouseData, i: number) => {
+                            
+                            const translateX = d.in_sf ?
+                              (0 + percentageX * sqFtSFScale(d.price_per_sqft))  :
+                              (props.width - 25 - (sqFtNYScale(d.price_per_sqft) * percentageX))
 
+                            const translateY = props.height - (elevationScale(d.elevation) * percentageY)
+                            
+                            const rotateAmount = d.in_sf ? 0 : 180
 
-        })
+                            return `translate(${translateX}, ${translateY}) rotate(${rotateAmount})`
+                          })
+                          .select('rect')
+                          .attr('width', (data: HouseData) => {
+                            if (transitionPhase.phaseIndex === 2) {
+                              const calcPercentageWidth = determineTriangleWith(data, percentageWidth)                             
+                              return Math.max((calcPercentageWidth * (props.width - 30) / 2), 6)
+                            }                            
+                            return Math.max((percentageWidth * (props.width - 30) / 2), 6)
+                          })
+                          .attr('fill', (data: HouseData) => data.in_sf ? 'green' : 'blue')
+                          .attr('opacity', (_, i) =>  i % 3 === 0 ? opacity : i % 2 ? opacity * 0.7 : opacity * 0.4 )
+                          .attr('rx', (data: HouseData) => {
+                            if (transitionPhase.phaseIndex === 2) {
+                              const calcPercentageWidth = determineTriangleWith(data, percentageWidth)                             
+                              const width = Math.max((calcPercentageWidth * (props.width - 30) / 2), 6)
+                              if (width === 6) {
+                                return width
+                              }
+                            }
+                            return roundedCorners
+                          })
+                          .attr('ry', (data: HouseData) => {
+                            if (transitionPhase.phaseIndex === 2) {
+                              const calcPercentageWidth = determineTriangleWith(data, percentageWidth)                             
+                              const width = Math.max((calcPercentageWidth * (props.width - 30) / 2), 6)
+                              if (width === 6) {
+                                return width
+                              }
+                            }
+                            return roundedCorners
+                          })
+                          .attr('class', 'boop')
+          }
+        )
     }
-  }, [transitionPhase, props, elevationScale, sqFtSFScale, sqFtNYScale, phaseColors, svgRef ])
+  }, [transitionPhase, props, elevationScale, sqFtSFScale, sqFtNYScale, phaseColors, svgRef, elevationMinMax ])
 
   return (
     <div style={{
