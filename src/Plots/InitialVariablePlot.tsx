8eae79279cedf81a38672d7dc6a451c84e302fda
Jdgronewold <jeff.gronewold@gmail.com>
2019-10-09 23:05:20 -0400

    boxes done

--- src/Plots/InitialVariablePlot.tsx
+++ src/Plots/InitialVariablePlot.tsx
@@ -14,7 +14,6 @@ interface InitialVariablePlotProps {
 
 export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: InitialVariablePlotProps) => {
   const svgRef = useRef(null)
-  const phaseColors = ['red', 'blue', 'green', 'purple']
   const elevationMinMax = d3.extent(props.data, (datum: HouseData) => datum.elevation) as [number, number]
   const sqFtMinMax = d3.extent(props.data, (datum: HouseData) => datum.price_per_sqft) as [number, number]
   const elevationScale = d3.scaleLinear().domain(elevationMinMax).range([0, props.height - 25])
@@ -31,14 +30,19 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
       const calculatedPhase = transitionHeights.findIndex((transitionHeight: number) => {
         return transitionHeight > currPos.y
       })
-      let phasePercentage: number
+
+      if (calculatedPhase === -1) {
+        setTransitionPhase({ phaseIndex: props.transitionHeights.length, phasePercentage: 1 })
+      } else {
+        let phasePercentage: number
       if (transitionHeights[calculatedPhase - 1]) {
         phasePercentage = 1 - ((transitionHeights[calculatedPhase] - currPos.y) / (transitionHeights[calculatedPhase] - transitionHeights[calculatedPhase - 1]))  
-      } else {
-        phasePercentage = 1
+        } else {
+          phasePercentage = 1
+        }
+        
+        setTransitionPhase({ phaseIndex: calculatedPhase, phasePercentage: Math.min(1, phasePercentage) })
       }
-      
-      setTransitionPhase({ phaseIndex: calculatedPhase, phasePercentage: Math.min(1, phasePercentage) })
     }, [], null, true, 10
   )
 
@@ -66,6 +70,11 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
         .select('g')
         .attr('class', 'container')
         .attr('transform', 'translate(25, 25)')
+
+      const boxContainer = svgElement
+        .select('g.box-container')
+        .attr('class', 'box-container')
+        .attr('transform', 'translate(25, 25)')
       
       const percentageY = transitionPhase.phaseIndex === 1 ? transitionPhase.phasePercentage : 1
 
@@ -73,18 +82,24 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
         transitionPhase.phasePercentage :
         transitionPhase.phaseIndex > 2 ? 0 : 1
       
-      const percentageX = transitionPhase.phaseIndex >= 3 ? transitionPhase.phasePercentage : 0
+      const percentageX = transitionPhase.phaseIndex === 3 ?
+        transitionPhase.phasePercentage :
+        transitionPhase.phaseIndex > 3 ? 1 : 0
 
       const roundedCorners = transitionPhase.phaseIndex >= 3 ? 6 : 0
       
       const opacity = transitionPhase.phaseIndex < 1 ? 0 : 0.8
+
+      const boxOpacity = transitionPhase.phaseIndex > 3 ? 0.25 : 0
+      const boxSizePercentage = transitionPhase.phaseIndex > 3 ? transitionPhase.phasePercentage : 0
       
       containerElement.selectAll('g')
         .data(props.data, (data: HouseData, i: number) => `${data.elevation} + ${i}`)
         .join((enter) => {
           return enter.append('g')
                       .append('rect')   
-                      .attr('height', 6)       
+                      .attr('height', 6)
+                      .attr('class', (data: HouseData) => `${data.elevation}+${data.price_per_sqft}`)       
                     },
           (update) => {
             return update.attr('transform', (d: HouseData, i: number) => {
@@ -93,7 +108,7 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
                               (0 + percentageX * sqFtSFScale(d.price_per_sqft))  :
                               (props.width - 25 - (sqFtNYScale(d.price_per_sqft) * percentageX))
 
-                            const translateY = props.height - (elevationScale(d.elevation) * percentageY)
+                            const translateY = (props.height - 25) - (elevationScale(d.elevation) * percentageY)
                             
                             const rotateAmount = d.in_sf ? 0 : 180
 
@@ -130,11 +145,37 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
                             }
                             return roundedCorners
                           })
-                          .attr('class', 'boop')
           }
         )
+
+      boxContainer.selectAll('g')
+          .data(['green', 'blue'], (d: string) => d)
+          .join((enter) => {
+            return enter
+              .append('g')
+              .append('rect')
+              .attr('transform', (d: string) => {
+                const isSF = d === 'green'
+                return `translate(${isSF ? 0 : sqFtSFScale(2205)}, ${isSF ? (props.height - 25) - elevationScale(31):  props.height -25}) rotate(-90)`
+              })
+              .attr('rotateY', 180)
+          },
+          (update) => {
+            return update.select('rect').attr('fill', (data: string) => data)
+              .attr('width', (data: string) => {
+                const isSF = data === 'green'
+                const maxSFHeight = (props.height - 25) - elevationScale(31) / (props.height - 25)
+                return isSF ? maxSFHeight * boxSizePercentage : elevationScale(31)
+              })
+              .attr('height', (data: string) => {
+                const isSF = data === 'green'
+                const maxNYWidth = (props.width - 25) - sqFtSFScale(2205) / (props.width - 25)
+                return isSF ? props.width - 25 : maxNYWidth * boxSizePercentage 
+              })
+              .attr('opacity', boxOpacity)
+          })
     }
-  }, [transitionPhase, props, elevationScale, sqFtSFScale, sqFtNYScale, phaseColors, svgRef, elevationMinMax ])
+  }, [transitionPhase, props, elevationScale, sqFtSFScale, sqFtNYScale, svgRef, elevationMinMax, sqFtMinMax ])
 
   return (
     <div style={{
@@ -145,6 +186,7 @@ export const InitialVariablePlot: React.FC<InitialVariablePlotProps> = (props: I
     }}>
       <svg ref={svgRef}>
         <g></g>
+        <g className="box-container"></g>
       </svg>
     </div>
   )
